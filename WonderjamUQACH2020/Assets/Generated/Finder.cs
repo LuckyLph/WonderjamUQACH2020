387ddafdf2f44af83be057af6c5cce4ffd3d8b59
Louis-Philippe Hudon <lp.hudon@hotmail.com>
2020-02-28 02:08:13 -0500

    First commit

---
+++ WonderjamUQACH2020/Assets/Generated/Finder.cs
@@ -0,0 +1,79 @@
+// ----- AUTO GENERATED CODE - ANY MODIFICATION WILL BE OVERRIDEN ----- //
+// ----- GENERATED ON 2020-02-28 01:56:29.273120600 -05:00 ----- //
+
+/*
+ * To add an object to the Finder, add a "Findable" attribute to the class :
+ *
+ * For example :
+ *
+ *   [Findable("MainController")]
+ *   public class MyClass : MonoBehaviour
+ *   {
+ *   }
+ *
+ * You can also use the generated constants. For example :
+ *
+ *   [Findable(R.S.Tag.MainController)]
+ *   public class MyClass : MonoBehaviour
+ *   {
+ *   }
+ *
+ * Please note that the "Findable" attribute is defined in this file.
+ */
+
+using System;
+using UnityEngine;
+
+namespace Harmony
+{
+    public static class Finder
+    {
+        private static Harmony.SqLiteConnectionFactory findableSqLiteConnectionFactory = null; //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\Assets\Libraries\Harmony\Scripts\Playmode\Database\SqLiteConnectionFactory.cs, line 13.
+        private static Harmony.NavigationMesh findableNavigationMesh = null; //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\Assets\Libraries\Harmony\Scripts\Playmode\Navigation\NavigationMesh.cs, line 12.
+        private static Harmony.PathFinder findablePathFinder = null; //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\Assets\Libraries\Harmony\Scripts\Playmode\Navigation\PathFinder.cs, line 13.
+    
+        public static Harmony.SqLiteConnectionFactory SqLiteConnectionFactory
+        {
+            get
+            {
+                if (!findableSqLiteConnectionFactory) findableSqLiteConnectionFactory = FindWithTag<Harmony.SqLiteConnectionFactory>(R.S.Tag.GameController);
+                return findableSqLiteConnectionFactory;
+            }
+        }
+        
+        public static Harmony.NavigationMesh NavigationMesh
+        {
+            get
+            {
+                if (!findableNavigationMesh) findableNavigationMesh = FindWithTag<Harmony.NavigationMesh>("NavigationMesh");
+                return findableNavigationMesh;
+            }
+        }
+        
+        public static Harmony.PathFinder PathFinder
+        {
+            get
+            {
+                if (!findablePathFinder) findablePathFinder = FindWithTag<Harmony.PathFinder>("NavigationMesh");
+                return findablePathFinder;
+            }
+        }
+        
+
+        private static T FindWithTag<T>(string tag)
+        {
+            return GameObject.FindWithTag(tag).GetComponent<T>();
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class FindableAttribute : PropertyAttribute
+    {
+        public string Tag { get; }
+
+        public FindableAttribute(string tag)
+        {
+            Tag = tag;
+        }
+    }
+}
---
+++ WonderjamUQACH2020/Assets/Generated/R.cs
@@ -0,0 +1,80 @@
+// ----- AUTO GENERATED CODE - ANY MODIFICATION WILL BE OVERRIDEN ----- //
+// ----- GENERATED ON 2020-02-28 01:56:29.266614200 -05:00 ----- //
+
+/*
+ * This file contains constants from :
+ *  - Tag names.
+ *  - Layer names.
+ *  - Scene names.
+ *  - Prefab names.
+ *  - GameObject names.
+ *  - AnimatorParameter names.
+ *
+ * Invalid names are listed in the "R.E" class. Here are the rules :
+ *  - Non-alphanumerical characters (like spaces) are prohibited.
+ *  - Starting with a non alphabetic character is prohibited.
+ *  - Per C# language rules, using the same name as it's class is prohibited. Ex : "GameObject", "Tag" or "Layer".
+ */
+
+namespace Harmony
+{
+    public static class R
+    {
+        public static class E
+        {
+            public enum Tag
+            {
+                Finish = 64315233, //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset.
+                Respawn = 74615215, //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset.
+                Untagged = 75014779, //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset.
+                GameController = 58339470, //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset.
+                EditorOnly = 71402144, //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset.
+                MainCamera = 38632605, //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset.
+                Player = 37192756, //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset.
+            }
+            public enum Layer
+            {
+                Water = 17379352, //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset, line 12.
+                //Ignore Raycast is invalid. File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset, line 10.
+                Default = 61255271, //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset, line 8.
+                TransparentFX = 15778512, //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset, line 9.
+                UI = 5562427, //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset, line 13.
+            }
+            public enum Scene
+            {
+                SampleScene = 52822372, //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\Assets\Scenes\SampleScene.unity.
+            }
+            public enum GameObject
+            {
+                //Main Camera is invalid. File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\Assets\Scenes\SampleScene.unity, line 127.
+            }
+        }
+        public static class S
+        {
+            public static class Tag
+            {
+                public const string Finish = "Finish"; //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset.
+                public const string Respawn = "Respawn"; //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset.
+                public const string Untagged = "Untagged"; //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset.
+                public const string GameController = "GameController"; //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset.
+                public const string EditorOnly = "EditorOnly"; //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset.
+                public const string MainCamera = "MainCamera"; //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset.
+                public const string Player = "Player"; //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset.
+            }
+            public static class Layer
+            {
+                public const string Water = "Water"; //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset, line 12.
+                public const string Default = "Default"; //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset, line 8.
+                public const string TransparentFX = "TransparentFX"; //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset, line 9.
+                public const string UI = "UI"; //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\ProjectSettings\TagManager.asset, line 13.
+            }
+            public static class Scene
+            {
+                public const string SampleScene = "SampleScene"; //File C:\Users\lphud\Documents\Projects\WonderjamUQACH2020\WonderjamUQACH2020\Assets\Scenes\SampleScene.unity.
+            }
+            public static class GameObject
+            {
+            }
+        }
+    }
+}
