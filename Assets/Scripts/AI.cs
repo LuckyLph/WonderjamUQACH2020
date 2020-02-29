96e17ca5610af3edcf86beb5046699ee70f9a8e7
Frédéric <fredicach@hotmail.com>
2020-02-29 00:23:43 -0500

    Nav mesh, pathing a* et debut attaque

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+  public GameObject attack;
+
+  private void OnTriggerEnter2D(Collider2D player)
+  {
+    Debug.Log("collider detected");
+    GameObject a = Instantiate(attack) as GameObject;
+  }
+}
