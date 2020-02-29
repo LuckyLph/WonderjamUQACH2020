55ea54c8fd3f32c1b6b87c904b5d0d825de4ec09
Axelexa <axel.zerbin@isen.yncrea.fr>
2020-02-29 15:49:11 +0100

    ajout de readme pour les 2 puzzles

---
+++ Assets/Prefabs/puzzles/offering/readme.cs
@@ -0,0 +1,29 @@
+﻿/**
+ * 
+ * Utilisation :
+ * 
+ * Placé le prefab d'exemple dans la pièce concerné
+ * Déplacer les zones de spawn (cristaux) et de placements (emplacements de cristaux) aux lieux désiré.
+ * Ajuster la Scale pour délimiter les zones de spawns
+ * Lorsque tous les cristaux sont correctements placé, le prefab d'exemple met son booléen "puzzleSolved" à TRUE
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
--- Assets/Prefabs/puzzles/plate order/readme.cs
+++ Assets/Prefabs/puzzles/plate order/readme.cs
@@ -1,18 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class readme : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+﻿/**
+ * 
+ * Utilisation :
+ * 
+ * Placé le prefab d'exemple dans la pièce concerné
+ * Ajouter ou supprimer des "plate" en enfant du prefab (autant que l'on souhaite, copier-coller celels présente ou ajouter le prefab)
+ * Placer les plates indépendamment aux endroits souhaité dans la salle
+ * Lorsque toutes les plates sont correctements activer, le prefab d'exemple met son booléen "puzzleSolved" à TRUE
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
