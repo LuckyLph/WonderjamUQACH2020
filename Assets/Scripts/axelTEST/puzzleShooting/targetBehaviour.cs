02a08d073658164068211677ced477fb0f675d32
Axelexa <axel.zerbin@isen.yncrea.fr>
2020-02-29 16:45:34 +0100

    puzzleshooting

---
+++ Assets/Scripts/axelTEST/puzzleShooting/targetBehaviour.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetBehaviour : MonoBehaviour
+{
+    private ShootPuzzleBehaviour parentScript;
+    private bool m_isActive = false;
+    public int activationTimeInSec = 1;
+    [SerializeField]
+    private int remainingTimeInSec;
+    public bool isActive
+    {
+        get
+        {
+            return this.m_isActive;
+        }
+        set
+        {
+            this.m_isActive = value;
+            if(value == true)
+            {
+                this.TellParent();
+            }
+        }
+    }
+
+    private SpriteRenderer whiteChild1;
+    private SpriteRenderer whiteChild2;
+
+    void Awake()
+    {
+        this.parentScript = this.GetComponentInParent<ShootPuzzleBehaviour>();
+        this.whiteChild1 = this.transform.GetChild(0).GetComponent<SpriteRenderer>();
+        this.whiteChild2 = this.transform.GetChild(2).GetComponent<SpriteRenderer>();
+    }
+
+    private void Activate()
+    {
+        this.isActive = true;
+        this.whiteChild1.color = Color.green;
+        this.whiteChild2.color = Color.green;
+    }
+
+    private void Deactivate()
+    {
+        this.isActive = false;
+        this.whiteChild1.color = Color.white;
+        this.whiteChild2.color = Color.white;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (!this.isActive)
+        {
+            this.remainingTimeInSec = this.activationTimeInSec;
+            this.Activate();
+            StartCoroutine(this.startTimer());
+        }
+    }
+
+    private void TellParent()
+    {
+
+    }
+
+    IEnumerator startTimer()
+    {
+        for (int i = 0; i <= this.remainingTimeInSec; this.remainingTimeInSec--)
+        {
+            yield return new WaitForSeconds(1);
+        }
+        this.Deactivate();
+    }
+}
