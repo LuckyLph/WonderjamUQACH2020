d77ca80f1b55d541af2fa17032cc60eeca8540d9
Axelexa <axel.zerbin@isen.yncrea.fr>
2020-02-29 01:18:49 +0100

    Ajout de quelques sons + puzzle plates

---
+++ Assets/Scripts/axelTEST/PlateBehavior.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private bool isActive;
+
+    private SpriteRenderer render;
+    private puzzlePlateBehavior puzzle;
+
+    void Awake()
+    {
+        this.render = this.gameObject.GetComponent<SpriteRenderer>();
+        this.puzzle = this.GetComponentInParent<puzzlePlateBehavior>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision !");
+        if(collision.gameObject.tag == "Player" && !this.isActive)
+        {
+            this.activate();
+        }
+    }
+
+
+
+
+
+
+
+    public void activate()
+    {
+        this.isActive = true;
+        this.render.color = Color.blue;
+        this.tellParent();
+    }
+
+    public void deactivate()
+    {
+        this.isActive = false;
+        this.render.color = Color.red;
+    }
+
+    private void tellParent()
+    {
+        puzzle.buttonActivated(this.gameObject);
+    }
+
+
+}
