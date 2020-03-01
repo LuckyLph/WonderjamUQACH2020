43eee21fcb83e2df6284eb802cb8d3f535c86e86
Thaison Dang <thaisondang@hotmail.fr>
2020-03-01 14:33:28 -0500

    Ajout shotgun and rifle and uncomment triggercollision

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -21,24 +21,24 @@ public class Bullet : MonoBehaviour
         Destroy(gameObject, player.weapons[player.index].bulletTime);
     }
 
-    /*private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
-            collision.gameObject.GetComponent<Enemy>().TakeDamage(player.varWeapon[player.index].bulletDamage);
+            collision.gameObject.GetComponent<Enemy>().TakeDamage(player.weapons[player.index].bulletDamage);
             Destroy(gameObject);
         }
         else if (collision.gameObject.tag == "Target")
         {
             targetBehaviour target = collision.gameObject.GetComponent<targetBehaviour>();
-            target.TakeDamage(player.varWeapon[player.index].bulletDamage);
+            target.TakeDamage(player.weapons[player.index].bulletDamage);
             Destroy(gameObject);
         }
         else if (collision.gameObject.tag == "Walls" || collision.gameObject.tag == "Target")
         {
             Destroy(gameObject);
         }
-    }*/
+    }
     
     void OnCollisionEnter2D(Collision2D other){
         if(other.gameObject.tag == "Enemy")
