b754182bffb24cdd87d968e61a1a76bdd1d47bbe
Frédéric <fredicach@hotmail.com>
2020-02-29 21:38:42 -0500

    Spawner ennemie, et drop fait. Manque que l'intéraction du jouer avec les drops

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,27 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public int health = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public int health = 0;
+  public GameObject dropMunition;
+  public GameObject dropVie;
+  private GameManager gameManager;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  // Update is called once per frame
+  void Awake()
+  {
+    gameManager = FindObjectOfType<GameManager>();
+  }
 
-    public void TakeDamage(int damage){
-        this.health -= damage;
-        if (this.health <= 0)
-        {
-            Destroy(this.gameObject);
-        }
+  public void TakeDamage(int damage)
+  {
+    health -= damage;
+    if (health <= 0)
+    {
+      Destroy(gameObject);
+      DropMunition();
+      DropVie();
     }
+  }
+
+  private void DropMunition()
+  {
+    float rand = Random.value;
+    if (rand <= gameManager.CoeficientSpawnDropMunition)
+    {
+      GameObject d = Instantiate(dropMunition) as GameObject;
+      d.transform.position = transform.position;
+    }
+  }
+
+  private void DropVie()
+  {
+    float rand = Random.value;
+    if (rand <= gameManager.CoeficientSpawnDropVie)
+    {
+      GameObject d = Instantiate(dropVie) as GameObject;
+      d.transform.position = transform.position;
+    }
+  }
 }
